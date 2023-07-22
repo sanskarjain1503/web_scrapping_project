77bee3ed421ca9681f0043abd16f6d6f8e0c5400
sanskarjain1503 <sanskarjain1503@gmail.com>
2023-07-22 14:39:18 +0530

    app to application

--- application.py
+++ application.py
@@ -16,6 +16,7 @@ def homepage():
     return render_template("index.html")
 
 @app.route("/review" , methods = ['POST' , 'GET'])
+@cross_origin()
 def index():
     if request.method == 'POST':
         try:
@@ -93,4 +94,4 @@ def index():
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="127.0.0.1",port=8000,debug=True)
